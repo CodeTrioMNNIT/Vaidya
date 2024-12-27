5777ee8d1708fa9ff1570a0c40bd97272ecaa731
vanshtyagi04 <vanshtyagi4526@gmail.com>
2024-12-27 22:59:41 +0530

    home page made

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from '../components/Header.jsx'
+import SpecialityMenu from '../components/SpecialityMenu.jsx'
+import Banner from '../components/Banner.jsx'
 
 const Home = () => {
   return (
     <div>
-        
+        <Header />
+        <SpecialityMenu />
     </div>
   )
 }
