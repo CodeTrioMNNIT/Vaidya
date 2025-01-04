200131e9786a6bea1f394e26a867ef5ab51cb1a4
Ayush-Gupta-bit <ag6700717@gmail.com>
2025-01-04 17:41:31 +0530

    Implement admin dashboard data retrieval and display in the dashboard component

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -12,6 +12,7 @@ export const AdminContextProvider = (props) => {
 
     const [doctors, setDoctors] = useState([])
     const [appointments, setAppointments] = useState([])
+    const [dashData, setDashData] = useState(false)
 
     const getAllDoctors = async () => {
         try {
@@ -92,8 +93,27 @@ export const AdminContextProvider = (props) => {
         }
     }
 
+    const getDashData = async () => {
+        try {
+            
+            const {data} = await axios.get(backendUrL+'/api/admin/dashboard', {headers: { 
+                Authorization: `${aToken}` 
+            }})
+
+            if(data.success) {
+                setDashData(data.dashData)
+                console.log(data.dashData)
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
     const value = {
-        aToken, setAToken, backendUrL, doctors, getAllDoctors, changeAvailability, appointments, setAppointments, getAllAppointments, cancelAppointment
+        aToken, setAToken, backendUrL, doctors, getAllDoctors, changeAvailability, appointments, setAppointments, getAllAppointments, cancelAppointment, dashData, getDashData
     }
 
     return(
--- backend/src/controllers/adminController.js
+++ backend/src/controllers/adminController.js
@@ -4,6 +4,7 @@ import bcrypt from "bcrypt"
 import { v2 as cloudinary } from "cloudinary"
 import fs from "fs"
 import doctorModel from "../models/doctorModel.js"
+import userModel from "../models/userModel.js"
 import jwt from "jsonwebtoken"
 import appointmentModel from '../models/appointmentModel.js'
 
@@ -137,5 +138,28 @@ const appointmentCancel = asyncHandler(async(req ,res) => {
     })
 })
 
+const adminDashboard = async (req, res) => {
 
-export { addDoctor, adminLogin, allDoctors, appointmentsAdmin, appointmentCancel }
+    try {
+        
+        const doctors = await doctorModel.find({})
+        const users = await userModel.find({})
+        const appointments = await appointmentModel.find({})
+
+        const dashData = {
+            doctors: doctors.length, 
+            appointments: appointments.length, 
+            patients: users.length, 
+            latestAppointments: appointments.reverse().slice(0, 5)
+        }
+
+        res.json({success:true, dashData})
+
+    } catch (error) {
+        console.log(error) 
+        res.json({success:false, message: error.message})
+    }
+
+} 
+
+export { addDoctor, adminLogin, allDoctors, appointmentsAdmin, appointmentCancel, adminDashboard }
